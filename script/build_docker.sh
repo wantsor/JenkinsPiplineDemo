5b8c002affdebe7da384a7355b9b9d310ca30af1
wantsor <liuxian3@asiainfo.com>
2018-08-11 11:43:55 +0800

    add docker build

--- script/build_docker.sh
+++ script/build_docker.sh
@@ -9,8 +9,9 @@ export DOCKER_TLS_VERIFY="1"
 export DOCKER_HOST="tcp://master1g7.cs-cn-hangzhou.aliyun.com:23193"
 export DOCKER_CERT_PATH="/Users/liuxian/workspace/yunmengWorkspace/dockerSpace/certFiles"
 
-/usr/local/bin/docker images
+docker images
 
 pwd
 cd ./dockerbuild
-pwd
+# build to aliyun docker
+docker build -t registry.cn-hangzhou.aliyuncs.com/ywsj-test/ywsj-wantsor/liux-test-helloworld-node:0.1 .
